9a4eaa16e9619212021496c03f5d280f77009aba
JoaoQuaresma <joao.quaresma1991@gmail.com>
2018-08-25 19:14:25 +0100

    Add completed ability
    completed? added to model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,17 +3,13 @@
 # Tasks controller
 class TasksController < ApplicationController
   before_action :set_project
+  before_action :set_task, only: %i[destroy complete]
   def create
     @task = @project.tasks.create(task_params)
     redirect_to @project
   end
 
-  def new
-
-  end
-
   def destroy
-    @task = @project.tasks.find(params[:id])
     if @task.destroy
       flash[:success] = "Task was deleted"
     else
@@ -22,10 +18,19 @@ class TasksController < ApplicationController
     redirect_to @project
   end
 
+  def complete
+    @task.update_attribute(:completed_at, Time.now)
+    redirect_to @project, notice: "Task Completed"
+  end
+
   private
 
   def set_project
     @project = Project.find(params[:project_id])
+  end
+
+  def set_task
+    @task = @project.tasks.find(params[:id])
   end
 
   def task_params
