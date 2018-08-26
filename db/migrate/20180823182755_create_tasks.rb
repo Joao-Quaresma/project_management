12b336c1f8b34192b06c88306fced66aec40b892
JoaoQuaresma <joao.quaresma1991@gmail.com>
2018-08-26 22:21:52 +0100

    Edit style and structure
    Small edit in styling and structure

--- db/migrate/20180823182755_create_tasks.rb
+++ db/migrate/20180823182755_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :content
-      t.references :project, foreign_key: true
+      t.references :project, index: true, foreign_key: true
 
       t.timestamps
     end
